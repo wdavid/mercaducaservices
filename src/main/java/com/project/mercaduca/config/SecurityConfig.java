3a8a1e5eda4ff0584d227710d369662af348af30
wdavid <00135222@uca.edu.sv>
2025-06-13 18:20:25 -0600

    changue

--- src/main/java/com/project/mercaduca/config/SecurityConfig.java
+++ src/main/java/com/project/mercaduca/config/SecurityConfig.java
@@ -86,7 +86,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:4200", "https://mercaduca-front.vercel.app"));
+        config.setAllowedOrigins(List.of("http://localhost:4200", "http://localhost:3000", "https://mercaduca-front.vercel.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
